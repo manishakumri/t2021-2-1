79d0528952e0913e4251962d857220911cc6f708
manishakumri <66863235+manishakumri@users.noreply.github.com>
2021-10-21 18:26:57 +0530

    init
    Problem-4: Get the total count of number list in the dictionary which is multiple of [1,2,3,4,5,6,7,8,9]
    (examples)
    input: [1,2,8,9,12,46,76,82,15,20,30]
    Output:
    {1: 11, 2: 8, 3: 4, 4: 4, 5: 3, 6: 2, 7: 0, 8: 1, 9: 1}

---
+++ Program-4.py
@@ -0,0 +1,11 @@
+l=[1,2,3,4,5,6,7,8,9]
+d={1:0,2:0,3:0,4:0,5:0,6:0,7:0,8:0,9:0}
+n = input()
+In = []
+for i in range(int(n)):
+    In.append(int(input()))
+for i in l:
+    for j in In:
+        if(j%i ==0):
+            d[i]+=1
+print (d)

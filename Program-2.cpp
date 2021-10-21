3b3da0ac676f74dfbf91842c045bfefbb28d454e
manishakumri <66863235+manishakumri@users.noreply.github.com>
2021-10-21 18:49:59 +0530

    init

--- Program-2.cpp
+++ Program-2.cpp
@@ -5,8 +5,9 @@ int main(){
     int n;
     cout<<"Enter a number ";
     cin>>n;
+    cout<<",";
     for (int i=1;i<=n;i++){
-            cout<<(2*i)-1<<" ";
+            cout<<","<<(2*i)-1;
     }
     return 0;
 
